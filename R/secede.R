d010aa12a10decf9f36ca21092576ad3408aa6a2
boB Rudis <bob@rud.is>
2017-05-27 15:32:21 -0400

    fixed link to topojson

--- R/secede.R
+++ R/secede.R
@@ -1,23 +1,23 @@
-library(rvest)
-library(dplyr)
-library(tidyr)
-library(gpclib)
 library(rgeos)
 library(sp)
 library(maptools)
-library(rgdal) # needs gdal > 1.11.0
-library(ggplot2)
-library(reshape2)
+library(mapproj)
+library(rgdal)
+library(grid)
 library(gridExtra)
+library(svglite)
+library(rvest)
+library(tidyverse)
 
 # extract data from rvested <div>'s and clean it up a bit
 # pass in the rvested HTML object and the CSS selector to extract
 
-extractAndCleanup <- function(data, selector, make_numeric=FALSE) {
-  x <- data %>% html_nodes(selector) %>% html_text()
-  x <- gsub("^[[:punct:][:space:]]*|[[:punct:][:space:]]*$", "", x)
-  if (make_numeric) x <- as.numeric(gsub("[,[:space:]]*", "", x))
-  x
+extract_and_cleanup <- function(data, selector, make_numeric=FALSE) {
+  data %>%
+    html_nodes(selector) %>%
+    html_text() %>%
+    gsub("^[[:punct:][:space:]]*|[[:punct:][:space:]]*$", "", .) -> x
+  if (make_numeric) as.numeric(gsub("[,[:space:]]*", "", x)) else x
 }
 
 # if we've already grabbed the data, then don't waste bandwidth
@@ -25,18 +25,18 @@ extractAndCleanup <- function(data, selector, make_numeric=FALSE) {
 if (!file.exists("data/secede.rda")) {
 
   # grab the BBC voting site HTML
-  bbc_vote <- html("http://www.bbc.com/news/events/scotland-decides/results")
+  bbc_vote <- read_html("http://www.bbc.com/news/events/scotland-decides/results")
 
   # make the data frame by capturing the data in the <div>s, converting some to
   # numeric and leaving others as text. we can simplify the data frame creation
   # code by putting the rvest field extraction code into one fucnction
 
-  secede <- data.frame(
-    council=bbc_vote %>% extractAndCleanup(".body-row__cell--council"),
-    electorate=bbc_vote %>% extractAndCleanup(".body-row__cell--electorate", TRUE),
-    yes=bbc_vote %>% extractAndCleanup(".body-row__cell--yes", TRUE),
-    no=bbc_vote %>% extractAndCleanup(".body-row__cell--no", TRUE),
-    stringsAsFactors=FALSE)
+  secede <- data_frame(
+    council = extract_and_cleanup(bbc_vote, ".body-row__cell--council"),
+    electorate = extract_and_cleanup(bbc_vote, ".body-row__cell--electorate", TRUE),
+    yes = extract_and_cleanup(bbc_vote, ".body-row__cell--yes", TRUE),
+    no = extract_and_cleanup(bbc_vote, ".body-row__cell--no", TRUE)
+  )
 
   # we may use the "gone" column at a later date so keep it
   # the BBC colors were horrible, so we use a muted Union Jack red for the
@@ -44,8 +44,7 @@ if (!file.exists("data/secede.rda")) {
   # we'll also use hex colors so they can be readily applied to base
   # graphics maps (if we choose to use base graphis)
 
-  secede <- secede %>% mutate(gone=yes>no,
-                              color=ifelse(gone, "#0065BD", "#CF142B77"))
+  secede <- mutate(secede, gone = (yes > no), color = ifelse(gone, "#0065BD", "#CF142B77"))
 
   # normalize the Council names to match the ones in the TopoJSON file
 
@@ -70,7 +69,7 @@ if (!file.exists("data/secede.rda")) {
 # we'll not do this if we've already done it, too
 
 if (!file.exists("data/scotland.json")) {
-  GET("http://static.bbci.co.uk/news/1.46.4-1166/js/data/maps/l/scotland-elections.js",
+  GET("http://static.bbci.co.uk/news/1.194.01570/js/data/maps/l/scotland-elections.js",
       write_disk("data/scotland.json"), progress())
   tmp <- readLines("data/scotland.json")
   writeLines(tmp[2], "data/scotland.json")
@@ -78,15 +77,15 @@ if (!file.exists("data/scotland.json")) {
 
 # read in our new shape file
 
-map = readOGR("data/scotland.json", "scotland-elections")
+election_map <- readOGR("data/scotland.json", "scotland-elections")
 
 # we have to sort our data frame by the order of the Council names
 # from the TopoJSON file since we're using base graphics to do the
 # plotting (we're using base since it's quick and easy and it didn't
 # work cleanly in ggplot when i tried it :-)
 
-secede$council <- factor(secede$council, map@data$name, ordered=TRUE)
-secede <- secede %>% arrange(council)
+mutate(secede, council = factor(council, election_map@data$NAME, ordered=TRUE)) %>%
+  arrange(council) -> secede
 
 
 # due to TopoJSON errors, base graphics is the easiest solution
@@ -100,13 +99,14 @@ secede <- secede %>% arrange(council)
 # one polygon in this TopoJSON file. So, we work around it by merging our
 # Council names and secession data with the fortified map data frame
 
-map_df <- fortify(map)
+map_df <- fortify(election_map)
 
 # manually associate the map id's with the Council names and vote data
-councils <- data.frame(id=0:(length(map@data$name)-1),
-                       council=as.character(map@data$name))
-map_df <- merge(map_df, councils, by="id")
-map_df <- merge(map_df, secede, by="council")
+councils <- data.frame(id = as.character(0:(length(election_map@data$NAME)-1)),
+                       council = as.character(election_map@data$NAME))
+
+left_join(map_df, councils) %>%
+  left_join(secede) -> map_df
 
 # now we can work with the map using ggplot
 
@@ -127,27 +127,25 @@ gg <- gg + theme(panel.border=element_blank())
 gg <- gg + theme(axis.ticks=element_blank())
 gg <- gg + theme(axis.text=element_blank())
 
-secede_m <- secede %>%
+secede %>%
   gather(variable, value, -council) %>%
   filter(variable %in% c("yes", "no")) %>%
-  mutate(value=as.numeric(value))
+  mutate(value=as.numeric(value)) -> secede_m
 
 # get things sorted to the bars display properly
 
-secede_m <- merge(secede_m, secede, by="council")
-secede_m$variable <- factor(secede_m$variable,
-                            levels=c("yes", "no"), ordered=TRUE)
-secede_m <- secede_m %>% arrange(no, variable)
-secede_m$council <- factor(secede_m$council,
-                           unique(secede_m$council), ordered=TRUE)
+secede_m <- left_join(secede_m, secede, by="council")
+secede_m$variable <- factor(secede_m$variable, levels=c("yes", "no"))
+secede_m <- arrange(secede_m, no, variable)
+secede_m$council <- factor(secede_m$council, unique(secede_m$council))
 
 gg1 <- ggplot(secede_m, aes(x=council, y=value, fill=factor(variable)))
 gg1 <- gg1 + geom_bar(stat="identity", position="fill")
 gg1 <- gg1 + scale_fill_manual(values=rev(unique(secede$color)),
-                             labels=c("Yes", "No"), name="Secede?")
+                               labels=c("Yes", "No"), name="Secede?")
 gg1 <- gg1 + geom_hline(yintercept=0.50, color="gray80")
-gg1 <- gg1 + geom_text(aes(label=percent(yes/100)), y=0.08, color="white", size=3)
-gg1 <- gg1 + geom_text(aes(label=percent(no/100)), y=0.92, color="white", size=3)
+gg1 <- gg1 + geom_text(aes(label=scales::percent(yes/100)), y=0.08, color="white", size=3)
+gg1 <- gg1 + geom_text(aes(label=scales::percent(no/100)), y=0.92, color="white", size=3)
 gg1 <- gg1 + coord_flip()
 gg1 <- gg1 + labs(x="", y="")
 gg1 <- gg1 + theme_bw()
@@ -158,6 +156,6 @@ gg1 <- gg1 + theme(axis.ticks=element_blank())
 gg1 <- gg1 + theme(axis.text.x=element_blank())
 
 vote <- arrangeGrob(gg1, gg, ncol=2,
-                     main=textGrob("Scotland Votes", gp=gpar(fontsize=20)))
+                    top=textGrob("Scotland Votes", gp=gpar(fontsize=20)))
 
 ggsave("plots/vote.svg", vote, width=9, height=6.5)
